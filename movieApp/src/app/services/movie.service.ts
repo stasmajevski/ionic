2e7b0145a9451ab560de66c81c7e71a6b8cc9547
stasmaj <s.majevski@cointraffic.io>
2019-04-08 22:33:28 +0300

    Connected to movie API

--- movieApp/src/app/services/movie.service.ts
+++ movieApp/src/app/services/movie.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+ 
+// Typescript custom enum for search types
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
-  constructor() { }
+  url = 'http://www.omdbapi.com/';
+  apiKey = '3e8e343b';
+ 
+  /**
+   * Constructor of the Service with Dependency Injection
+   * @param http The standard Angular HttpClient to make requests
+   */
+  constructor(private http: HttpClient) { }
+ 
+  /**
+  * Get data from the OmdbApi 
+  */
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results => results['Search'])
+    );
+  }
+ 
+  /**
+  * Get the detailed information for an ID using the "i" parameter
+  */
+  getDetails(id) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
 }
