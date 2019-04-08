5c76e319ae58456d61c4d070e3a43b16f30f4304
stasmaj <s.majevski@cointraffic.io>
2019-04-08 23:13:54 +0300

    Added: movie details controller

--- movieApp/src/app/pages/movie-details/movie-details.page.ts
+++ movieApp/src/app/pages/movie-details/movie-details.page.ts
@@ -1,15 +1,34 @@
+import { MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+ 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
-
-  constructor() { }
-
+ 
+  information = null;
+ 
+  /**
+   * Constructor of our details page
+   * @param activatedRoute Information about the route we are on
+   * @param movieService The movie Service to get data
+   */
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
+ 
   ngOnInit() {
+    // Get the ID that was passed with the URL
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+ 
+    // Get the information from the API
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
   }
-
+ 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
 }
