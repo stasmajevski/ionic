3ea938b65e9762c0af2dfcd049108ec0ecc1c3c1
stasmaj <s.majevski@cointraffic.io>
2019-04-08 22:41:21 +0300

    Added: movies page controller

--- movieApp/src/app/pages/movies/movies.page.ts
+++ movieApp/src/app/pages/movies/movies.page.ts
@@ -1,15 +1,28 @@
+import { MovieService, SearchType } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+ 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
 })
 export class MoviesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+ 
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+ 
+  /**
+   * Constructor of our first page
+   * @param movieService The movie Service to get data
+   */
+  constructor(private movieService: MovieService) { }
+ 
+  ngOnInit() { }
+ 
+  searchChanged() {
+    // Call our service function which returns an Observable
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
   }
-
 }
